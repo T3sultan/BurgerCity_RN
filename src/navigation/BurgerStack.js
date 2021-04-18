d4c542ecac3de56b18183ef34d5e06cfe3d8a688
T3sultan <incognito.soul011@gmail.com>
2021-04-18 15:44:59 +0600

    delivery

--- src/navigation/BurgerStack.js
+++ src/navigation/BurgerStack.js
@@ -2,25 +2,15 @@ import React from 'react';
 import {View,Text} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '../screens/home/HomeScreen';
+import BurgerScreen from '../screens/burgers/BurgerScreen';
 import HeaderTitle from '../components/HeaderTitle';
-import OrderMethod from '../screens/burgers/OrderMethod';
-import DeliveryAddress from '../screens/burgers/DeliveryAddress';
-import DeliveryAddressConfirmed from '../screens/burgers/DeliveryAddressConfirmed';
 
 
 const BurgerStack = ({params})=>{
     const Stack = createStackNavigator();
     return (
-       <Stack.Navigator initialRouteName="OrderMethod">
-           <Stack.Screen name="OrderMethod" component={OrderMethod}
-               options={{headerTitle:props=><HeaderTitle/>}}
-           />
-           <Stack.Screen
-               name="DeliveryAddress" component={DeliveryAddress}
-               options={{headerTitle:props=><HeaderTitle/>}}
-           />
-           <Stack.Screen
-               name="DeliveryAddressConfirmed" component={DeliveryAddressConfirmed}
+       <Stack.Navigator>
+           <Stack.Screen name="BurgerScreen" component={BurgerScreen}
                options={{headerTitle:props=><HeaderTitle/>}}
            />
         </Stack.Navigator>
